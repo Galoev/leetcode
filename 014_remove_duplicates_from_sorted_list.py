bd0dfbc54c4378d746f8f1ff3b4f188cf21a7c4e
Galoev <galoyev.ilkin@gmail.com>
2021-12-12 20:49:41 +0300

    add #14 solution v2

--- 014_remove_duplicates_from_sorted_list.py
+++ 014_remove_duplicates_from_sorted_list.py
@@ -8,6 +8,17 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head 
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
+
+    def deleteDuplicatesMy(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return head
         
