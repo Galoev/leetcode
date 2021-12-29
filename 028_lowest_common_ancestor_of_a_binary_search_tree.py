7443d1da7bc1e963c7978df31e27e454177dce8d
Galoev <galoyev.ilkin@gmail.com>
2021-12-29 19:47:40 +0300

    add #28 solution v2

--- 028_lowest_common_ancestor_of_a_binary_search_tree.py
+++ 028_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -9,30 +9,39 @@ from utils import Tree, TreeNode
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path_to_p = self.getPathToNode(root, p)
-        path_to_q = self.getPathToNode(root, q)
-        len_n = min(len(path_to_p), len(path_to_q))
-        last_common_node = path_to_p[0]
-        for i in range(1, len_n):
-            node_p = path_to_p[i]
-            node_q = path_to_q[i]
-            if node_p is not node_q:
-                break
+        node = root
+        while True:
+            if node.val < p.val and node.val < q.val:
+                node = node.right
+            elif node.val > p.val and node.val > q.val:
+                node = node.left
             else:
-                last_common_node = node_p
-        return last_common_node
+                return node
+        return None
+    #     path_to_p = self.getPathToNode(root, p)
+    #     path_to_q = self.getPathToNode(root, q)
+    #     len_n = min(len(path_to_p), len(path_to_q))
+    #     last_common_node = path_to_p[0]
+    #     for i in range(1, len_n):
+    #         node_p = path_to_p[i]
+    #         node_q = path_to_q[i]
+    #         if node_p is not node_q:
+    #             break
+    #         else:
+    #             last_common_node = node_p
+    #     return last_common_node
 
     
-    def getPathToNode(self, root: TreeNode, target: TreeNode) -> list:
-        node = root
-        path_to_target = [node]
-        while node.val != target.val:
-            if node.val < target.val:
-                node = node.right
-            elif node.val > target.val:
-                node = node.left
-            path_to_target.append(node)
-        return path_to_target
+    # def getPathToNode(self, root: TreeNode, target: TreeNode) -> list:
+    #     node = root
+    #     path_to_target = [node]
+    #     while node.val != target.val:
+    #         if node.val < target.val:
+    #             node = node.right
+    #         elif node.val > target.val:
+    #             node = node.left
+    #         path_to_target.append(node)
+    #     return path_to_target
 
 if __name__ == "__main__":
     root = Tree.createTree([6,2,8,0,4,7,9,"null","null",3,5])
