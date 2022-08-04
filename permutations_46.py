128e09c7260768cb67890d6bef8239e7259ff8db
Ilkin Galoev <galoev@MacBook-Air-Ilkin.local>
2022-08-04 18:58:29 +0300

    add 46 solution

---
+++ permutations_46.py
@@ -0,0 +1,20 @@
+from typing import List
+from unicodedata import name
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums]
+
+        res = []
+        for i in range(len(nums)):
+            for subset in self.permute(nums[0:i] + nums[i+1:]):
+                res.append([nums[i]] + subset)
+
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.permute([1, 2, 3]))
