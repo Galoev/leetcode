8a3d79614056c90daa720db2caa987012956e021
Galoev <galoyev.ilkin@gmail.com>
2023-02-21 12:42:15 +0300

    add problem 461 solution

---
+++ 461_hamming-distance.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return bin(x ^ y).count("1")
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.hammingDistance(1, 4))
