eab4367c7e8f3f450caa75e235b2b6f40b48888a
Galoev <galoyev.ilkin@gmail.com>
2023-02-28 10:36:31 +0300

    add problem 2392 solution

---
+++ 392_is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        for c in t:
+            if i >= len(s):
+                return True
+            
+            if s[i] == c:
+                i += 1
+        
+        if i == len(s):
+            return True
+        
+        return False
