7196904972638820c333de9e031dcf847d7677fc
Galoev <galoyev.ilkin@gmail.com>
2023-03-06 11:48:58 +0300

    add problem 409 solution

--- 409_longest-palindrome.py
+++ 409_longest-palindrome.py
@@ -1,2 +1,16 @@
+from collections import Counter
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> int:
+        letters_count = Counter(s)
+        res = 0
+        odd = False
+        for v in letters_count.values():
+            if v % 2 == 0:
+                res += v
+            else:
+                res += v - 1
+                odd = True
+            
+        return res + 1 if odd else res
